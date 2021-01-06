633410eb137a223b9640d2dc2263310fdd31b794
Amitava Haldar <onno.haldar@icloud.com>
2021-01-06 15:23:23 +0100

    refactor

--- src/test/post-ontology.ts
+++ src/test/post-ontology.ts
@@ -4,12 +4,28 @@
  */
 
 import * as FormData from 'form-data';
-//import { request } from 'http';
+import { request } from 'http';
 import { createReadStream } from 'fs';
- 
-const readStream = createReadStream('./test-ontology');
+
+console.log(__dirname);
+const readStream = createReadStream('./test-ontology.ttl');
  
 const form = new FormData();
 form.append('', readStream);
 
 console.log(form.getHeaders());
+
+const req = request(
+    {
+      host: 'localhost',
+      port: '3030',
+      path: '/dgwnu/upload',
+      method: 'POST',
+      headers: form.getHeaders(),
+    },
+    response => {
+      console.log(response.statusCode); // 200
+    }
+  );
+   
+form.pipe(req);
